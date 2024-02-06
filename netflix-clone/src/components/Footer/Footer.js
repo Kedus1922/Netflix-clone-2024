4869f42dae52756d7960319d05e652c27c03d886
Kedus1922 <152933553+Kedus1922@users.noreply.github.com>
2024-02-06 18:46:24 -0500

    footer

--- netflix-clone/src/components/Footer/Footer.js
+++ netflix-clone/src/components/Footer/Footer.js
@@ -10,26 +10,41 @@ function Footer() {
       <div className='footer_inner_container'>
         <div className='footer_icons'>
           <FacebookOutlinedIcon />
+          <br />
           <InstagramIcon />
+          <br />
           <TwitterIcon />
+          <br />
           <YouTubeIcon />
+          <br />
+          <br />
+          <br />
         </div>
         <div className='footer_data'>
           <div>
+          
             <ul>
               <li>Audio Description</li>
+              <br />
               <li>Investor Relations</li>
+              <br />
               <li>Privacy</li>
+              <br />
               <li>Contact Us</li>
+              <br />
             </ul>
           </div>
 
           <div>
             <ul>
               <li>Help Center</li>
+              <br />
               <li>Jobs</li>
+              <br />
               <li>Legal Notices</li>
+              <br />
               <li>Do NOt Sell Or Share My Personal Information</li>
+              <br />
 
             </ul>
           </div>
@@ -37,8 +52,11 @@ function Footer() {
           <div>
             <ul>
               <li>Gift Cards</li>
+              <br />
               <li>Netflix Shop</li>
+              <br />
               <li>Cookie Preferences</li>
+              <br />
               <li>Ad Choices</li>
               
 
@@ -48,8 +66,11 @@ function Footer() {
           <div>
             <ul>
               <li>Media Center</li>
+              <br />
               <li>Terms of Use</li>
+              <br />
               <li>Corporate Information</li>
+              <br />
 
             </ul>
           </div>
@@ -68,6 +89,10 @@ function Footer() {
 
           <div className='copy-write'>
             &copy; 1997-2024 Netflix, inc
+            <br />
+            <br />
+            <br />
+            <br />
 
           </div>
       </div>
